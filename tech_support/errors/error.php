2b522c9291a109204f856339ef4685aa1348dfe4
HasanVision <hasan.visionx@gmail.com>
2024-09-25 13:13:50 -0400

    display error message and clears it

--- tech_support/errors/error.php
+++ tech_support/errors/error.php
@@ -1,6 +1,18 @@
-<?php include '../view/header.php'; ?>
+<?php
+session_start();
+include '../view/header.php';
+?>
 <main>
     <h1>Error</h1>
-    <p><?php echo $error; ?></p>
+    <p>
+        <?php 
+        if (isset($_SESSION['error_message'])) {
+            echo htmlspecialchars($_SESSION['error_message']);
+            unset($_SESSION['error_message']); 
+        } else {
+            echo "An unknown error occurred.";
+        }
+        ?>
+    </p>
 </main>
 <?php include '../view/footer.php'; ?>
